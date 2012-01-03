beae39f47862b14f5b748f858287808c2f95b28e
unknown <Brett@Brett-Notebook.(none)>
2012-01-03 18:33:59 -0500

    Validation and Tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)$}i,
+    message: 'Must be a gif, jpg, or png image URL'
+  }
 end
