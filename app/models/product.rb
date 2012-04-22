3236883df19736b31978aae03439ef17eb6c76d3
Brett Koenig <brett@myhost.(none)>
2012-04-22 18:01:13 -0400

    Added partials

---
+++ app/models/product.rb
@@ -0,0 +1,22 @@
+class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)$}i,
+    message: 'Must be a gif, jpg, or png image URL'
+  }
+  
+  private
+    #ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, "Line Items present")
+        return false
+      end
+    end
+end
